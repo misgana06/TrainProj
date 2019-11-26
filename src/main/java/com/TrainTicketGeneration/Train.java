95e1e62f55148a5db119acda51ef193474f6ecb3
Fred <mtezera@gmail.com>
2019-11-26 14:33:10 -0500

    test

---
+++ src/main/java/com/TrainTicketGeneration/Train.java
@@ -0,0 +1,11 @@
+package com.TrainTicketGeneration;
+
+public class Train {
+
+    private int trainNumber;
+    private String trainName;
+    private String sourceStation;
+    private String destStation;
+    private String 
+
+}
